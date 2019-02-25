1d384a1a842a5e9b951b80dd8384d6bd792a3e41
Emil Haugen <emilhau@student.matnat.uio.no>
2019-02-25 12:21:23 +0100

    updating lasso

--- Matlab/lasso_sparse_coding.m
+++ Matlab/lasso_sparse_coding.m
@@ -1,5 +1,11 @@
-function outputX = lasso_sparse_coding(U, D, X)
-%Get sparse codes for vectors x^t (columns in X) by solving 
-%LASSO with least angle regression (LARS), keeping D fixed
-outputX = X^2;
+function X = lasso_sparse_coding(U, D)
+    %Get sparse codes for vectors u^t (columns in U) by solving 
+    %LASSO with least angle regression (LARS), keeping D fixed
+    
+    Udim = size(U);
+    k = Udim(1);
+    for j = 1:2
+        b = lasso(D, U(:,j));
+    end 
+    X = b;
 end
