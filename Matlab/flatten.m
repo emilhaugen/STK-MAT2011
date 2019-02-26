e329d89d8b3a8f7f1f4b1b456aa2dd0da5e05b56
Emil Haugen <emilhau@student.matnat.uio.no>
2019-02-26 18:46:27 +0100

    moving files utility/preprocess functions

--- Matlab/flatten.m
+++
@@ -1,7 +0,0 @@
-function B = flatten(A)
-    %FLATTEN helper method to flatten matrix to 1D vector
-    %flatten 2d-array to 1d
-    AT = transpose(A); %transpose to flatten by row
-    B = AT(:);
-end
-
