c3c41e02b7a59a03f0ebf0f760e9aaab79170718
Emil Haugen <emilhau@student.matnat.uio.no>
2019-02-26 18:52:43 +0100

    silly bugs

--- Matlab/Help_Functions/dictionary_update_util.m
+++ Matlab/Help_Functions/dictionary_update_util.m
@@ -2,15 +2,18 @@ function [D, D_prev] = dictionary_update_util(D_prev, A, B)
     % DICTIONARY_UPDATE_UTIL utility function called by update_dictionary()
     %   
     % 
+    % Return D (matrix): updated dictionary matrix
+    %
+    % Return D_prev (matrix): input dictionary matrix, unchanged.
     %
     
     if abs(min(diag(A))) < 1e-10
         error("Error: Zero diagonal element in A")
     
-    CODE_LEN = length(D(1,:));
-    D_prev = D;
+    CODE_LEN = length(D_prev(1,:));
+    D = D_prev;
     for j = 1:CODE_LEN %iterate over columns of D
-        dj = B(:,j) - D*A(:,j) + D(:,j)*A(j,j); 
+        dj = B(:,j) - D_prev*A(:,j) + D_prev(:,j)*A(j,j); 
         D(:,j) = dj / (norm(dj) * A(j,j));
     end 
 end
