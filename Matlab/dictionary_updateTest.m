cad8405bb8e0ae6c9c4c77121512049f4b968831
Emil Haugen <emilhau@student.matnat.uio.no>
2019-02-26 20:14:36 +0100

    more testing, tidying up script

--- Matlab/dictionary_updateTest.m
+++ Matlab/dictionary_updateTest.m
@@ -1,60 +1,75 @@
 %
-%  Script to test dictionary_update function.
+%  Script to test dictionary_update function. Not much to see.
 %  
+%   Manually testing alternating optimization between 
+%       LASSO and BCD (block coord. descent)
 %
 %
 %
-%
-%
-addpath("Utility_Functions");
+addpath("Help_Functions"); %make help functions available
 addpath("Preprocess");
 
-
+%set constants
+FILENAME = "Restoration.txt";
 SUBSET_LEN = 64;
 BLOCK_LEN = 8; %data subset will have SUBSET_LEN^2 / BLOCK_LEN^2 vectors
 CODE_LEN = 100; %length of sparse code vectors, no. of colums in dictionary
 
-U = ascii_to_data_matrix("Restoration.txt", SUBSET_LEN, BLOCK_LEN);
+%read ascii data
+U = ascii_to_data_matrix(FILENAME, SUBSET_LEN, BLOCK_LEN);
 Udim = size(U);
-m = Udim(1);
 T = Udim(2); %using notation T for no. of data vectors as in paper
 
+%randomly initialize dictionary
 D = init_dict(U, CODE_LEN);
- 
-tol = 1e-6;
-max_iter = 5;
 
-
+%get initial LASSO estimates for X
 X = zeros(CODE_LEN, T);
 X = lasso_sparse_coding(U, D, X, T);
 A = sum_of_outer_prods(X, X);
 B = sum_of_outer_prods(U, X);
 
-if abs(min(diag(A))) < 1e-9
-    error("Error: Zero diagonal element in A")
-end 
-%%{
-for i = 1:1
-    D_prev = D;
-    for j = 1:CODE_LEN %initial update
-        dj = B(:,j) - D*A(:,j) + D(:,j)*A(j,j); 
-        D(:,j) = dj / (norm(dj) * A(j,j));
-    end 
-end    
+%test update_deictionary() function
+tol = 1e-9;
+max_iter = 50;
+[D, D_prev, updated, num_iter] = dictionary_update(D, A, B, tol, max_iter);
 
-   
+
+%evaluate results of dictionary update  
+histogram(D)
+max(max(D))
+norm(D, "fro")
+norm(D - D_prev, "fro")
+nnz(updated)
+num_iter
+
+%repeat LASSO
+X = lasso_sparse_coding(U, D, X, T);
+A = sum_of_outer_prods(X, X);
+B = sum_of_outer_prods(U, X);
+
+%repeat update_dictionary()
+[D, D_prev, updated, num_iter] = dictionary_update(D, A, B, tol, max_iter);
+
+%evaluate results of dictionary update  
+histogram(D)
+max(max(D))
+norm(D, "fro")
+norm(D - D_prev, "fro")
+nnz(updated)
+num_iter
+
 %{
 norm(D - D_prev, "fro")
 max(max(D))
 max(max(D_prev))
-%}
 z = zeros(T);
 z = z(:, 1);
 for p = 1:T
     z(p) = CODE_LEN - nnz(X(:,p));
 end    
 z    
-%%}
+%}
 
 
 
