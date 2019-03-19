e07a69fd99a024867d892281887aed2c1a5c2506
Emil Haugen <emilhau@student.matnat.uio.no>
2019-03-19 17:35:51 +0100

    function to plot atoms

---
+++ Matlab/plot_dictionary.m
@@ -0,0 +1,29 @@
+function plot_dictionary(D, V, BLOCK_LEN, directory)
+    % PLOT_DICTIONARY plot atoms of dictionary
+    % 
+    %   param D (matrix): dictionary
+    %
+    %   param V (matrix): eigenvectors to map PCA dictionary back to 
+    %                      original format. If already on original format,
+    %                      use identity matrix.
+    %   
+    %   param BLOCK_LEN (int): side length of square patches needed for
+    %                           reconstruction
+    %
+    %   param directory (string): location to save plots
+    % 
+    %   return NOTHING
+    
+    
+    num_atoms = length(D(1,:)); % No. of atoms/columns in dictionary
+    rec_dict = V*D; % Reconstructed dictionary
+    
+    for j = 1:num_atoms
+        patch = reshape(rec_dict(:,j), BLOCK_LEN, BLOCK_LEN)';
+        fig = imagesc(patch);
+        colorbar;
+        title(sprintf("Atom no. %d of %d", j, num_atoms));
+        saveas(fig, strcat(directory, sprintf("Atom%d.png", j)));
+    end    
+end
+
